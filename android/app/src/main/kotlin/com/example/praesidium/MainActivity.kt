3e16f9ce484ae2f278d612d6c04ef7ff58183c1f
Bradley <bradleyjesseb@gmail.com>
2023-09-06 21:45:28 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/praesidium/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.praesidium
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  final String title = "Praesidium";
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height - kToolbarHeight;
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Container(
+        height: height,
+        width: width,
+        color: Colors.pink,
+        child: Row(
+          children: [
+            Container(
+              color: Colors.white,
+              width: width * 0.25,
+              child: const Column(
+                children: [Text("Dashboard")],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
