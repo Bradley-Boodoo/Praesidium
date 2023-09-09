0d09bc3c1c6c3e1556c217031eb253204a917da7
Bradley <bradleyjesseb@gmail.com>
2023-09-09 02:00:58 -0300

    renamed from table_header

---
+++ lib/screens/home/components/projects_table.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+import '../../../theme.dart';
+
+class ProjectsTable extends StatefulWidget {
+  final double tableColumnWidth;
+
+  const ProjectsTable({super.key, required this.tableColumnWidth});
+
+  @override
+  State<ProjectsTable> createState() => _TableState();
+}
+
+class _TableState extends State<ProjectsTable> {
+  List<bool> isHoverList = List.generate(5, (_) => false);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          /* HEADINGS */
+          Container(
+            height: 25,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                    width: widget.tableColumnWidth,
+                    child: const Text("Project Name")),
+                SizedBox(
+                    width: widget.tableColumnWidth,
+                    child: const Text("Start Date")),
+                SizedBox(
+                    width: widget.tableColumnWidth,
+                    child: const Text("End Date")),
+              ],
+            ),
+          ),
+
+          // CONTENT
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return MouseRegion(
+                  onHover: (event) {
+                    setState(() {
+                      isHoverList[index] = true;
+                    });
+                  },
+                  onExit: (event) {
+                    setState(() {
+                      isHoverList[index] = false;
+                    });
+                  },
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      // Hover Styling
+                      decoration: BoxDecoration(
+                        color: isHoverList[index]
+                            ? kAccentColor
+                            : kBackgroundColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                            width: widget.tableColumnWidth,
+                            child: const Text("Build SAPA"),
+                          ),
+                          SizedBox(
+                            width: widget.tableColumnWidth,
+                            child: const Text("11th September 2023"),
+                          ),
+                          SizedBox(
+                            width: widget.tableColumnWidth,
+                            child: const Text("20th April 2024"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
