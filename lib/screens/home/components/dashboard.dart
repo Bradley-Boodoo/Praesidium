a42c7e4f193a83df1273504de42ec2a0300b4c50
Bradley <bradleyjesseb@gmail.com>
2023-09-09 02:01:22 -0300

    added padding

--- lib/screens/home/components/dashboard.dart
+++ lib/screens/home/components/dashboard.dart
@@ -11,33 +11,36 @@ class Dashboard extends StatelessWidget {
     return Container(
       color: kAccentColor,
       width: dashboardWidth,
-      child: Column(
-        children: [
-          const Text("Dashboard"),
-          const Text("Project"),
-          const Text("Project Description"),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text("Dashboard"),
+            const Text("Project"),
+            const Text("Project Description"),
 
-          /* PROJECT STAGES */
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-                onPressed: () {}, child: const Text("Project Stages")),
-          ),
+            /* PROJECT STAGES */
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text("Project Stages")),
+            ),
 
-          /* PROJECT TASKS */
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-                onPressed: () {}, child: const Text("Project Tasks")),
-          ),
+            /* PROJECT TASKS */
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text("Project Tasks")),
+            ),
 
-          /* PROJECT RISKS */
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-                onPressed: () {}, child: const Text("Project Risks")),
-          ),
-        ],
+            /* PROJECT RISKS */
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text("Project Risks")),
+            ),
+          ],
+        ),
       ),
     );
   }
